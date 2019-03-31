dcd9698142db9471a641cf8730fa9c28b10e7d16
Vartan Benohanian <vartanbeno@gmail.com>
2019-03-31 01:51:57 -0400

    Add decode functions for list and dict. Add tests
    Signed-off-by: Vartan Benohanian <vartanbeno@gmail.com>

--- bencode/__init__.py
+++ bencode/__init__.py
@@ -1,56 +1,106 @@
+from collections import OrderedDict
+
 from bencode.exceptions import DecodeException
 
 
-def decode(b):
+def decode(b, start_index=0):
     try:
-        return decoding_functions[b[0]](b)
+        return decoding_functions[b[0]](b, start_index)
     except (IndexError, KeyError):
         raise DecodeException('Cannot decode value: invalid bencode provided.')
 
 
-def decode_string(b):
+def decode_string(b, start_index=0):
     """
     Decode a bencode into a string.
     E.g.: If b == '4:test', the method should return 'test'. The number 4 represents the length of the string 'test'.
     E.g.: If b == '11:hello world', the method should return 'hello world'. The space is accounted for in the length.
     :param b: the bencoded string
-    :return: the decoded string
+    :param start_index: the index from which we will start looking the colon (':') in the string
+    :return: the decoded string, new start_index
     """
     try:
-        colon_index = b.index(':')
-        length = int(b[:colon_index])
+        colon_index = b.index(':', start_index)
+        length = int(b[start_index:colon_index])
     except ValueError:
         raise DecodeException('Cannot decode string: bencode must start with length of string, followed by a colon.')
 
-    string = b[colon_index + 1:]
+    string = b[colon_index + 1:colon_index + 1 + length]
     if len(string) != length:
         raise DecodeException('Cannot decode string: invalid length for bencode.')
 
-    return string
+    return string, colon_index + len(string) + 1
 
 
-def decode_int(b):
+def decode_int(b, start_index=0):
     """
     Decode a bencode into an int.
     E.g.: If b == 'i12e', the method should return 12.
-    E.g.: If b == 'i43278593753845734985724e', the method should return 43278593753845734985724.
+    E.g.: If b == 'i-12e', the method should return -12.
     :param b: the bencoded string
-    :return: the decoded number
+    :param start_index: the index from which we will start looking the 'e' in the string
+    :return: the decoded number, new start_index
     """
     try:
-        if b[-1] != 'e':
-            raise DecodeException('Cannot decode int: bencode does not end with \'e\'.')
-        return int(b[1:-1])
+        e_index = b.index('e', start_index)
+        string = b[start_index + 1:e_index]
+
+        # Negative 0 not allowed.
+        # No leading zeroes, e.g. 007 is not allowed, it should be 7 instead.
+        # Zero is just 0, not 00, not 000, etc.
+        if len(string) > 1 and (string[0:2] == '-0' or (string[0] == '0' and string[1] != 'e')):
+            raise ValueError
+
+        return int(string), e_index + 1
     except (IndexError, ValueError):
         raise DecodeException('Cannot decode int: invalid bencode provided.')
 
 
-def decode_list(b):
-    pass
+def decode_list(b, start_index=0):
+    """
+    Decode a bencode into a list.
+    E.g.: If b == 'le', the method should return [].
+    E.g.: If b == 'li12ee', the method should return [12].
+    E.g.: If b == 'li12e4:i12e4:testlleee', the method should return [12, 'i12e', 'test', [[]]].
+    :param b: the bencoded string
+    :param start_index: the index at which we will check if the corresponding character is 'e'.
+    :return: the decoded list, new start_index
+    """
+    decoded_list = []
+    start_index += 1
+    try:
+        while b[start_index] != 'e':
+            content, start_index = decoding_functions[b[start_index]](b, start_index)
+            decoded_list.append(content)
+        return decoded_list, start_index + 1
+    except (IndexError, ValueError):
+        raise DecodeException('Cannot decode int: invalid bencode provided.')
 
 
-def decode_dictionary(b):
-    pass
+def decode_dictionary(b, start_index=0):
+    """
+    Decode a bencode into a dictionary.
+    E.g.: If b == 'de', the method should return an empty OrderedDict.
+    E.g.: If b == 'd4:test4:teste', the method should return an OrderedDict with k: 'test' => v: 'test'.
+    The dictionary is ordered alphabetically by key.
+    E.g.: If b == 'd4:test4:test1:a1:ae', the method should return an OrderedDict with k: 'a' => v: 'a', k: 'test' => v: 'test'.
+    :param b: the bencoded string
+    :param start_index: the index at which we will check if the corresponding character is 'e'.
+    :return: the decoded dictionary, new start_index
+    """
+    decoded_dictionary = OrderedDict()
+    start_index += 1
+    try:
+        while b[start_index] != 'e':
+            key, start_index = decoding_functions[b[start_index]](b, start_index)
+            if not isinstance(key, str):
+                raise DecodeException('Dictionary keys must be strings.')
+            decoded_dictionary[key], start_index = decoding_functions[b[start_index]](b, start_index)
+
+        # All keys must appear in lexicographical order, i.e. alphabetical order
+        return OrderedDict(sorted(decoded_dictionary.items())), start_index + 1
+    except (IndexError, ValueError):
+        raise DecodeException('Cannot decode int: invalid bencode provided.')
 
 
 """
@@ -76,4 +126,6 @@ decoding_functions = {
 
 
 if __name__ == '__main__':
-    print(decode('2:hi'))
+    print(decode('2:hi')[0])
+    print(decode('li12ei12e4:i12e4:test4:test8:hi worldllllleleeeeee')[0])
+    print(decode('d4:test4:test1:a1:b3:fooi12e1:cd4:test4:testee')[0])
---
+++ bencode/test_bencode.py
@@ -0,0 +1,100 @@
+from collections import OrderedDict
+from unittest import TestCase, main
+
+from bencode import decode, DecodeException
+
+
+class TestBencode(TestCase):
+
+    def test_decode_string_success(self):
+        foo, _ = decode('4:test')
+        self.assertEqual(foo, 'test')
+
+        foo, _ = decode('11:hello world')
+        self.assertEqual(foo, 'hello world')
+
+        foo, _ = decode('0:')
+        self.assertEqual(foo, '')
+
+        foo, _ = decode('4:i12e')
+        self.assertEqual(foo, 'i12e')
+
+    def test_decode_string_fail(self):
+        self.assertRaises(DecodeException, decode, '-1:test')
+        self.assertRaises(DecodeException, decode, 'test')
+        self.assertRaises(DecodeException, decode, '5:test')
+        self.assertRaises(DecodeException, decode, '5test')
+        self.assertRaises(DecodeException, decode, '1:')
+
+    def test_decode_int_success(self):
+        foo, _ = decode('i12e')
+        self.assertEqual(foo, 12)
+
+        foo, _ = decode('i0e')
+        self.assertEqual(foo, 0)
+
+        foo, _ = decode('i123456789e')
+        self.assertEqual(foo, 123456789)
+
+        foo, _ = decode('i-1e')
+        self.assertEqual(foo, -1)
+
+    def test_decode_int_fail(self):
+        self.assertRaises(DecodeException, decode, '7')
+        self.assertRaises(DecodeException, decode, 'i7')
+        self.assertRaises(DecodeException, decode, '7e')
+        self.assertRaises(DecodeException, decode, 'ie')
+        self.assertRaises(DecodeException, decode, 'i00e')
+        self.assertRaises(DecodeException, decode, 'i-0e')
+        self.assertRaises(DecodeException, decode, 'i07e')
+        self.assertRaises(DecodeException, decode, 'i--7e')
+
+    def test_decode_list_success(self):
+        foo, _ = decode('le')
+        self.assertEqual(foo, [])
+
+        foo, _ = decode('lllleeee')
+        self.assertEqual(foo, [[[[]]]])
+
+        foo, _ = decode('li12ee')
+        self.assertEqual(foo, [12])
+
+        foo, _ = decode('l4:testi12ei12e3:foolleee')
+        self.assertEqual(foo, ['test', 12, 12, 'foo', [[]]])
+
+        foo, _ = decode('l4:testd3:foo3:baree')
+        self.assertEqual(foo, ['test', OrderedDict([('foo', 'bar')])])
+
+    def test_decode_list_fail(self):
+        self.assertRaises(DecodeException, decode, 'l')
+        self.assertRaises(DecodeException, decode, 'e')
+        self.assertRaises(DecodeException, decode, 'liee')
+        self.assertRaises(DecodeException, decode, 'l4:e')
+
+    def test_decode_dictionary(self):
+        foo, _ = decode('de')
+        self.assertEqual(foo, OrderedDict())
+
+        foo, _ = decode('d4:test4:teste')
+        self.assertEqual(foo, OrderedDict([('test', 'test')]))
+
+        foo, _ = decode('d4:test4:test1:z1:ze')
+        self.assertEqual(foo, OrderedDict([('test', 'test'), ('z', 'z')]))
+
+        foo, _ = decode('d4:test4:test1:a1:ae')
+        self.assertEqual(foo, OrderedDict([('a', 'a'), ('test', 'test')]))
+
+        foo, _ = decode('d4:test4:test1:ad1:bdeee')
+        self.assertEqual(foo, OrderedDict([('a', OrderedDict([('b', OrderedDict())])), ('test', 'test')]))
+
+        foo, _ = decode('d1:ad1:bd1:cd1:dd1:edeeeeee')
+        self.assertEqual(foo, OrderedDict([('a', OrderedDict([('b', OrderedDict([('c', OrderedDict([('d', OrderedDict([('e', OrderedDict())]))]))]))]))]))
+
+    def test_decode_dictionary_fail(self):
+        self.assertRaises(DecodeException, decode, 'd')
+        self.assertRaises(DecodeException, decode, 'e')
+        self.assertRaises(DecodeException, decode, 'di12ei12ee')
+
+
+if __name__ == '__main__':
+    main()
